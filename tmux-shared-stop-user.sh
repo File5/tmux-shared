ea89e876f2823eb2637b191c632b0917414c1522
Aleksandr Zuev <zuev08@gmail.com>
2021-04-03 09:23:09 -0400

    Fix error messages when killing all sessions

--- tmux-shared-stop-user.sh
+++ tmux-shared-stop-user.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 USER=$(whoami)
-tmux -S /tmp/tmux-shared/$USER kill-session -t $USER
-tmux -S /tmp/tmux-shared/$USER kill-session -t shared
+tmux -S /tmp/tmux-shared/$USER kill-session -t $USER 2>/dev/null
+tmux -S /tmp/tmux-shared/$USER kill-session -t shared 2>/dev/null
