e36298fb82f7b93cd1fc168fab725b79d31e1763
Aleksandr Zuev <zuev08@gmail.com>
2021-04-04 01:34:15 -0400

    Window exists check, additional key bindings

--- tmux-shared-new-window.sh
+++ tmux-shared-new-window.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 USER=$(whoami)
 
+if tmux -S /tmp/tmux-shared/$USER has-session -t shared:$USER 2>/dev/null; then
+    tmux -S /tmp/tmux-shared/$USER attach -t shared
+    exit
+fi;
+
 tmux -S /tmp/tmux-shared/$USER new-window -t shared -n $USER
 
 i=0
@@ -31,6 +36,13 @@ else
     layout="even-horizontal"
 fi;
 tmux -S /tmp/tmux-shared/$USER select-layout -t shared $layout
+
+tmux -S /tmp/tmux-shared/$USER set -t shared prefix C-a
+tmux -S /tmp/tmux-shared/$USER bind C-a send-prefix
+tmux -S /tmp/tmux-shared/$USER set -t shared status-left-length 14
+tmux -S /tmp/tmux-shared/$USER set -t shared status-left "#{prefix} [#S] "
+tmux -S /tmp/tmux-shared/$USER set -t shared status-style fg=black,bg=cyan
+tmux -S /tmp/tmux-shared/$USER bind Tab if -F -t shared "#{==:#{session_name}:#{window_name},shared:$USER}" "display-message \"Can't swap pane with itself\"" "swap-pane -t $USER:0.0; select-window -t $USER"
 
 tmux -S /tmp/tmux-shared/$USER attach -t shared
 
